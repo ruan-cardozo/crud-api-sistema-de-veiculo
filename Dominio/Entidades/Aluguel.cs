43d43329a62545aee0bae706ac61532c45ea2e99
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-21 00:23:28 -0300

    feat: criando getters and setters para aluguel

--- Dominio/Entidades/Aluguel.cs
+++ Dominio/Entidades/Aluguel.cs
@@ -2,21 +2,39 @@ namespace Dominio.Entidades.Aluguel;
 
 public class Aluguel
 {
-    private int Id;
-    private DateOnly DataRetirada;
-    private TimeOnly HoraRetirada;
-    private double KmRetirada;
-    private double QtdLtsRetirada;
-    private DateOnly DataDevolução;
-    private TimeOnly HoraDevolução;
-    private double KmDevolução;
-    private double QtdLtsDevolução;
-    private DateOnly DataPrevista;
-    private TimeOnly HoraPrevista;
-    private decimal ValorPrevisto;
-    private decimal ValorReal;
-    private int Id_Veiculo;
-    private int Id_Cliente;
+    private int id;
+    private DateOnly dataRetirada;
+    private TimeOnly horaRetirada;
+    private double kmRetirada;
+    private double qtdLtsRetirada;
+    private DateOnly dataDevolução;
+    private TimeOnly horaDevolução;
+    private double kmDevolução;
+    private double qtdLtsDevolução;
+    private DateOnly dataPrevista;
+    private TimeOnly horaPrevista;
+    private decimal valorPrevisto;
+    private decimal valorReal;
+    private int id_Veiculo;
+    private int id_Cliente;
+
+    public int Id { get => id; private set => id = value; }
+    public DateOnly DataRetirada { get => dataRetirada; private set => dataRetirada = value; }
+    public TimeOnly HoraRetirada { get => horaRetirada; private set => horaRetirada = value; }
+    public double KmRetirada { get => kmRetirada; private set => kmRetirada = value; }
+    public double QtdLtsRetirada { get => qtdLtsRetirada; private set => qtdLtsRetirada = value; }
+    public DateOnly DataDevolução { get => dataDevolução; private set => dataDevolução = value; }
+    public TimeOnly HoraDevolução { get => horaDevolução; private set => horaDevolução = value; }
+    public double KmDevolução { get => kmDevolução; private set => kmDevolução = value; }
+    public double QtdLtsDevolução { get => qtdLtsDevolução; private set => qtdLtsDevolução = value; }
+    public DateOnly DataPrevista { get => dataPrevista; private set => dataPrevista = value; }
+    public TimeOnly HoraPrevista { get => horaPrevista; private set => horaPrevista = value; }
+    public decimal ValorPrevisto { get => valorPrevisto; private set => valorPrevisto = value; }
+    public decimal ValorReal { get => valorReal; private set => valorReal = value; }
+    public int Id_Veiculo { get => id_Veiculo; private set => id_Veiculo = value; }
+    public int Id_Cliente { get => id_Cliente; private set => id_Cliente = value; }
+
+
 
     public Aluguel(int id, DateOnly dataRetirada, TimeOnly horaRetirada, double kmRetirada, double qtdLtsRetirada, DateOnly dataDevolução, TimeOnly horaDevolução, double kmDevolução, double qtdLtsDevolução, DateOnly dataPrevista, TimeOnly horaPrevista, decimal valorPrevisto, decimal valorReal, int id_Veiculo, int id_Cliente)
     {
