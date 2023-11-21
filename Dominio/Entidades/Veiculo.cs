012cbaca87eea9fd280036912137622f4eb5f93b
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-20 23:43:47 -0300

    feat: criar entidade veiculo

---
+++ Dominio/Entidades/Veiculo.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Enums.ModeloEnum;
+using Dominio.Enums.MarcaEnum;
+
+namespace Dominio.Veiculo;
+public class Veiculo
+{
+    private string placa;
+    private Marca marca;
+    private Modelo modelo;
+    private int anoFab;
+    private double km;
+    private string numChassi;
+
+    public string Placa { get => placa; private set => placa = value; }
+    public Marca Marca { get => marca; private set => marca = value; }
+    public Modelo Modelo {get => modelo; private set => modelo = value; }
+    public int AnoFab {get => anoFab; private set => anoFab = value; }
+    public double Km {get => km; private set => km = value; }
+    public string NumChassi {get => numChassi; private set => numChassi = value; }
+    
+    public Veiculo(string placa, Marca marca, Modelo modelo, int anoFab, double km, string numChassi)
+    {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFab = anoFab;
+        this.km = km;
+        this.numChassi = numChassi;
+    }
+}
