432ab5f37e575ca6eef274183428a67e13de4cca
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-20 23:53:58 -0300

    refect: refatorar obrigatoriedade campos veiculo

--- Dominio/Entidades/Veiculo.cs
+++ Dominio/Entidades/Veiculo.cs
@@ -1,22 +1,22 @@
 ﻿using Domain.Enums.ModeloEnum;
 using Dominio.Enums.MarcaEnum;
 
-namespace Dominio.Veiculo;
+namespace Dominio.Entidades.Veiculo;
 public class Veiculo
 {
     private string placa;
     private Marca marca;
     private Modelo modelo;
-    private int anoFab;
+    private int? anoFab;
     private double km;
-    private string numChassi;
+    private string? numChassi;
 
     public string Placa { get => placa; private set => placa = value; }
     public Marca Marca { get => marca; private set => marca = value; }
     public Modelo Modelo {get => modelo; private set => modelo = value; }
-    public int AnoFab {get => anoFab; private set => anoFab = value; }
+    public int? AnoFab {get => anoFab; private set => anoFab = value; }
     public double Km {get => km; private set => km = value; }
-    public string NumChassi {get => numChassi; private set => numChassi = value; }
+    public string? NumChassi {get => numChassi; private set => numChassi = value; }
     
     public Veiculo(string placa, Marca marca, Modelo modelo, int anoFab, double km, string numChassi)
     {
