c4cb3c65fa7c116fd23f71290ab1622545611a3d
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-21 00:14:27 -0300

    feat: criar construtor para aluguel

--- Dominio/Entidades/Aluguel.cs
+++ Dominio/Entidades/Aluguel.cs
@@ -17,4 +17,23 @@ public class Aluguel
     private decimal ValorReal;
     private int Id_Veiculo;
     private int Id_Cliente;
+
+    public Aluguel(int id, DateOnly dataRetirada, TimeOnly horaRetirada, double kmRetirada, double qtdLtsRetirada, DateOnly dataDevolução, TimeOnly horaDevolução, double kmDevolução, double qtdLtsDevolução, DateOnly dataPrevista, TimeOnly horaPrevista, decimal valorPrevisto, decimal valorReal, int id_Veiculo, int id_Cliente)
+    {
+        Id = id;
+        DataRetirada = dataRetirada;
+        HoraRetirada = horaRetirada;
+        KmRetirada = kmRetirada;
+        QtdLtsRetirada = qtdLtsRetirada;
+        DataDevolução = dataDevolução;
+        HoraDevolução = horaDevolução;
+        KmDevolução = kmDevolução;
+        QtdLtsDevolução = qtdLtsDevolução;
+        DataPrevista = dataPrevista;
+        HoraPrevista = horaPrevista;
+        ValorPrevisto = valorPrevisto;
+        ValorReal = valorReal;
+        Id_Veiculo = id_Veiculo;
+        Id_Cliente = id_Cliente;
+    }
 }
