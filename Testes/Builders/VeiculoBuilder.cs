8f3605a5ece175eab694e4f7d58113fd48456afb
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-21 00:42:49 -0300

    feat: criar builder veiculo

--- Testes/Builders/VeiculoBuilder.cs
+++ Testes/Builders/VeiculoBuilder.cs
@@ -24,4 +24,64 @@ public class VeiculoBuilder
     {
         return new Veiculo(_placa, _marca, _modelo, _anoFab, _km, _numChassi);
     }
+
+    public VeiculoBuilder ComPlaca(string placa)
+    {
+        _placa = placa;
+        return this;
+    }
+
+    public VeiculoBuilder ComMarca(Marca marca)
+    {
+        _marca = marca;
+        return this;
+    }
+
+    public VeiculoBuilder ComModelo(Modelo modelo)
+    {
+        _modelo = modelo;
+        return this;
+    }
+
+    public VeiculoBuilder ComAnoFab(int? anoFab)
+    {
+        _anoFab = anoFab;
+        return this;
+    }
+
+    public VeiculoBuilder ComKm(double km)
+    {
+        _km = km;
+        return this;
+    }
+
+    public VeiculoBuilder ComNumChassi(string? numChassi)
+    {
+        _numChassi = numChassi;
+        return this;
+    }
+
+    public VeiculoBuilder ComValoresPadroes()
+    {
+        _placa = "HPS-0465";
+        _marca = Marca.BMW;
+        _modelo = Modelo.X6;
+        _anoFab = 2023;
+        _km = 0;
+        _numChassi = "59100316158";
+        return this;
+    }
+
+    public VeiculoBuilder ComValoresAleatorios()
+    {
+        var faker = new Faker();
+
+        _placa = faker.Random.String2(7, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+        _marca = faker.PickRandom<Marca>();
+        _modelo = faker.PickRandom<Modelo>();
+        _anoFab = faker.Random.Int(2000, 2021);
+        _km = faker.Random.Double(0, 100000);
+        _numChassi = faker.Random.String2(11, "0123456789");
+        return this;
+    }
 }
