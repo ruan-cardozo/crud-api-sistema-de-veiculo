396691fcacbb8b63c143c2ca6a37c28a54f97da2
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-23 17:33:17 -0300

    refact: retirar enums

--- Testes/Entidades/Veiculo.cs
+++ Testes/Entidades/Veiculo.cs
@@ -6,20 +6,20 @@ namespace Dominio.Entidades.Veiculo;
 public class Veiculo
 {
     private string placa;
-    private Marca marca;
-    private Modelo modelo;
+    private string marca;
+    private string modelo;
     private int anoFab;
-    private double km; //menor que 0
+    private double km; 
     private string numChassi;
 
     public string Placa { get => placa; private set => placa = value; }
-    public Marca Marca { get => marca; private set => marca = value; }
-    public Modelo Modelo {get => modelo; private set => modelo = value; }
+    public string Marca { get => marca; private set => marca = value; }
+    public string Modelo {get => modelo; private set => modelo = value; }
     public int AnoFab {get => anoFab; private set => anoFab = value; }
     public double Km {get => km; private set => km = value; }
     public string NumChassi {get => numChassi; private set => numChassi = value; }
     
-    public Veiculo(string placa, Marca marca, Modelo modelo, int anoFab, double km, string numChassi)
+    public Veiculo(string placa, string marca, string modelo, int anoFab, double km, string numChassi)
     {
         //defina regras para os valores das variaveis
         if (marca == null)
@@ -44,6 +44,7 @@ public class Veiculo
         
         if(string.IsNullOrEmpty(km.ToString()))
             throw new ArgumentException("Km não pode ser nulo ou vazio", nameof(km));
+
         this.placa = placa;
         this.marca = marca;
         this.modelo = modelo;
