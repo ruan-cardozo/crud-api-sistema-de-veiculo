daf0d34e221c249b87a2029a3d02bdda23f19c15
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-23 17:39:20 -0300

    remove: removendo imports e identação

--- Testes/Entidades/Veiculo.cs
+++ Testes/Entidades/Veiculo.cs
@@ -1,28 +1,24 @@
-﻿using Dominio.Enums.ModeloEnum;
-using Dominio.Enums.MarcaEnum;
-using Xunit.Sdk;
-
-namespace Dominio.Entidades.Veiculo;
+﻿namespace Dominio.Entidades.Veiculo;
 public class Veiculo
 {
     private string placa;
     private string marca;
     private string modelo;
     private int anoFab;
-    private double km; 
+    private double km;
     private string numChassi;
 
     public string Placa { get => placa; private set => placa = value; }
     public string Marca { get => marca; private set => marca = value; }
-    public string Modelo {get => modelo; private set => modelo = value; }
-    public int AnoFab {get => anoFab; private set => anoFab = value; }
-    public double Km {get => km; private set => km = value; }
-    public string NumChassi {get => numChassi; private set => numChassi = value; }
-    
+    public string Modelo { get => modelo; private set => modelo = value; }
+    public int AnoFab { get => anoFab; private set => anoFab = value; }
+    public double Km { get => km; private set => km = value; }
+    public string NumChassi { get => numChassi; private set => numChassi = value; }
+
     public Veiculo(string placa, string marca, string modelo, int anoFab, double km, string numChassi)
     {
         //defina regras para os valores das variaveis
-        if(string.IsNullOrEmpty(modelo))
+        if (string.IsNullOrEmpty(modelo))
             throw new ArgumentException("Modelo não pode ser nulo ou vazio", nameof(modelo));
         if (string.IsNullOrEmpty(marca))
             throw new ArgumentException("Marca não pode ser nula ou vazio", nameof(marca));
@@ -39,12 +35,12 @@ public class Veiculo
             throw new ArgumentException("Número do chassi não pode ser nulo ou vazio", nameof(numChassi));
 
         if (anoFab < 1900)
-            throw new ArgumentException("Ano de fabricação não pode ser menor que 1900", nameof(anoFab));    
+            throw new ArgumentException("Ano de fabricação não pode ser menor que 1900", nameof(anoFab));
 
         if (string.IsNullOrEmpty(placa))
             throw new ArgumentException("Placa não pode ser nula ou vazia", nameof(placa));
-        
-        if(string.IsNullOrEmpty(km.ToString()))
+
+        if (string.IsNullOrEmpty(km.ToString()))
             throw new ArgumentException("Km não pode ser nulo ou vazio", nameof(km));
 
         this.placa = placa;
