7f370ad0b043ba914ff2674309f12ff77ad90c7b
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-23 17:37:43 -0300

    removendo enums

--- Testes/Enums/MarcaEnum.cs
+++
@@ -1,8 +0,0 @@
-namespace Dominio.Enums.MarcaEnum;
-
-public enum Marca 
-{
-    Audi,
-    BMW,
-    FIAT
-}
--- Testes/ProdutoTeste/VeiculoTeste.cs
+++ Testes/ProdutoTeste/VeiculoTeste.cs
@@ -113,5 +113,7 @@ public class VeiculoTeste
         Assert.Throws<ArgumentException>(() => veiculoEsperado.ComNumChassi(numChassi));
     }
 
+
+    []
     
 }
