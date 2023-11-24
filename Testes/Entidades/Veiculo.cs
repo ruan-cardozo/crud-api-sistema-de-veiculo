4243402da566e2d24a0ee7d5e9da811e4bdc9450
ruan-cardozo <ruanvcardozo@gmail.com>
2023-11-24 09:06:30 -0300

    remove: enums

--- Testes/Entidades/Veiculo.cs
+++ Testes/Entidades/Veiculo.cs
@@ -6,20 +6,20 @@ namespace Dominio.Entidades.Veiculo;
 public class Veiculo
 {
     private string placa;
-    private Marca marca;
-    private Modelo modelo;
+    private string marca;
+    private string modelo;
     private int? anoFab;
     private double km; //menor que 0
     private string? numChassi;
 
     public string Placa { get => placa; private set => placa = value; }
-    public Marca Marca { get => marca; private set => marca = value; }
-    public Modelo Modelo {get => modelo; private set => modelo = value; }
+    public string Marca { get => marca; private set => marca = value; }
+    public string Modelo {get => modelo; private set => modelo = value; }
     public int? AnoFab {get => anoFab; private set => anoFab = value; }
     public double Km {get => km; private set => km = value; }
     public string? NumChassi {get => numChassi; private set => numChassi = value; }
     
-    public Veiculo(string placa, Marca marca, Modelo modelo, int? anoFab, double km, string numChassi)
+    public Veiculo(string placa, string marca, string modelo, int? anoFab, double km, string numChassi)
     {
         //defina regras para os valores das variaveis
         if (marca == null)
